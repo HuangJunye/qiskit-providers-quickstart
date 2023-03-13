4f31d99a4107425630a98f48cc0d1ddd602c03ab
Junye Huang <h.jun.ye@gmail.com>
2023-03-13 19:03:47 +0100

    add cuQuantum, ddsim to local simulators

---
+++ py_out/MQT DDSIM-bell-backend.py
@@ -0,0 +1,16 @@
+from mqt import ddsim
+provider = ddsim.DDSIMProvider()
+backend = provider.get_backend('qasm_simulator')
+
+# Build and transpile circuit
+from qiskit import QuantumCircuit, transpile
+circuit = QuantumCircuit(2, 2)
+circuit.h(0)
+circuit.cx(0,1)
+circuit.measure([0,1], [0,1])
+transpiled_circuit = transpile(circuit, backend)
+
+# Run the circuit and get result
+job = backend.run(transpiled_circuit)
+counts = job.result().get_counts()
+print(counts)
